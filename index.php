806fe14c3c729ee9d1547095c8a3a16c07d82549
anirudh786 <anirudhjain_sanjay@srmuniv.edu.in>
2018-08-05 21:35:27 +0530

    front end web page

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+   <head>
+     <meta charset="utf-8"/>
+       <title> ANIRUDH WEBPAGE- Forms & form elements </title>
+    </head>
+<body>
+  <form action="" method="post">
+       username:<input type="text" name="username" maxlength="10" size="10 "/>
+       password:<input type="password" name="username" maxlength="10" size="10 "/>
+
+     </form>  
+   </body>
+  </html>
